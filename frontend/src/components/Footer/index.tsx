920c23011cf544957cd77448e56c7bcd9edfc312
Augustotadeurodrigues <augusto.tadeu.rodrigues@gmail.com>
2021-11-05 10:10:32 -0300

    Basic Statis Components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/Augustotadeurodrigues" target="_blank" rel="noreferrer">Augusto Rodrigues</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/t_augustorodrigues" target="_blank" rel="noreferrer">@t_augustorodrigues</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
